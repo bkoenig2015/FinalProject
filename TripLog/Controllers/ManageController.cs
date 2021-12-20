d97605b404a29ce4e141b32f2efb8bb981a143ed
AndrewJhD <89947450+AndrewJhD@users.noreply.github.com>
2021-12-20 01:21:19 -0600

    cleaning up code

--- TripLog/Controllers/ManageController.cs
+++ TripLog/Controllers/ManageController.cs
@@ -43,10 +43,7 @@ namespace NoteApp.Controllers
             bool needsSave = false;
             string notifyMsg = "";
 
-            /******************************************************
-             * On delete, retrieve full entity from database first
-             * so have Name value for notification message
-             ******************************************************/
+            
 
             if (vm.Category.CategoryId > 0) {
                 vm.Category = data.Categories.Get(vm.Category.CategoryId);
@@ -54,26 +51,9 @@ namespace NoteApp.Controllers
                 notifyMsg = $"{notifyMsg} {vm.Category.Name}, ";
                 needsSave = true;
             }
-            /*
-            if (vm.Title.TitleId > 0) {
-                vm.Title = data.Titles.Get(vm.Title.TitleId);
-                data.Titles.Delete(vm.Title);
-                notifyMsg = $"{notifyMsg} {vm.Title.Name}, ";
-                needsSave = true;
-            }
-            if (vm.Description.DescriptionId > 0)
-            {
-                vm.Description = data.Descriptions.Get(vm.Description.DescriptionId);
-                data.Descriptions.Delete(vm.Description);
-                notifyMsg = $"{notifyMsg} {vm.Description.Name}, ";
-                needsSave = true;
-            }*/
-            /**************************************************************************************
-             * If try to delete a destination that's associated with a trip, will get an exception,
-             * bc FK delete behavior is set to Restrict. No exception for an accommodation, bc FK
-             * delete behavior is set to SetNull, and no exception for activity bc just removes 
-             * entry from linking table. So catch block is only concerned with a destination.
-             **************************************************************************************/
+
+
+            
             if (needsSave) {
                 try {
                     data.Save();
